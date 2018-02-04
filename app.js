0edc3420db08b64c6e38002ab1c55e8d8fb43784
Steina <steina.dogg@gmail.com>
2018-02-04 14:13:34 +0000

    changes to async logic

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 
 const express = require('express');
+
 const app = express();
-const fs = require('fs');
 const articles = require('./articles');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const person =  {name:"John", lastName:"Doe", age:50, eyeColor:"blue"};
-
-app.use('/static',express.static('public'))
 
 // Use subapp articles.
 app.use('/', articles);
--- articles.js
+++ articles.js
@@ -3,86 +3,105 @@
 const showdown = require('showdown');
 const frontmatter = require('front-matter');
 const fs = require('fs');
+
 const converter = new showdown.Converter();
 const util = require('util');
+
 const readdir = util.promisify(fs.readdir);
 const readFile = util.promisify(fs.readFile);
-const ejsLint = require('ejs-lint');
 
 // Server
 const express = require('express');
+
 const app = express();
 
-// Template engine
-const ejs = require('ejs')
+
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 
-app.use('/articles/img',express.static('./articles/img/'))
-
-app.get('/articles/:slug', async (req, res) => {
-    var filelist = await readdir('./articles');
-    filelist = filelist.filter(item => item.endsWith('.md'))
-
-    let found = false;
-    for(var i = 0; i < filelist.length; i++){
-        const data = await read('./articles/' + filelist[i]);
-        const fm = frontmatter(data);
-        let fileslug = fm.attributes.slug;
-        if (req.params.slug === fileslug){
-            let html = converter.makeHtml(fm.body)
-            res.render('article', {html, title: fm.attributes.title});
-            found = true;
-        }
-    }
-    if (!found){
-        res.status(404).render('error',{title : "Fannst ekki.",error : "Efnið fannst ekki."});
-    }
-
-});
-
-app.get('/', async (req, res) => {
-    var filelist = await readdir('./articles');
-    filelist = filelist.filter(item => item.endsWith('.md'))
-
-    var attrlist = [];
-    for(var i = 0; i < filelist.length; i++){
-        const data = await read('./articles/' + filelist[i]);
-        const fm = frontmatter(data);
-
-        let date = new Date(fm.attributes.date);
-        fm.attributes.date = date.toDateString();   
-       
-        attrlist[i] = fm.attributes;
-    }
-    res.render('frontpage.ejs', {articles: attrlist, title: 'Greinasafn'});
-});
-
-async function sort(attrlist){
-    attrlist.sort(async function(a,b){
-        var date1,date2;
-        date1 = await a.attributes.date;
-        date2 = await b.attributes.date;
-        if (date1 >= date2) return -1;
-        else return 1;
-    })
-}
+app.use('/static', express.static('public'));
+app.use('/articles/img', express.static('./articles/img/'));
 
 
+// Helpers
 async function read(file) {
     const data = await readFile(file);
     return data.toString();
 }
 
-  // Handle 404
-  app.use(function(req, res) {
-    res.render('error',{title : "Fannst ekki.",error : "Efnið fannst ekki."});
- });
- 
+
+// Routes
+app.get('/articles/:slug', async (req, res) => {
+    let filelist = await readdir('./articles');
+    filelist = filelist.filter(item => item.endsWith('.md'));
+
+    let promises = filelist.map(async (value) => {
+        return read(`./articles/${value}`);
+    })
+
+    let found = false;
+    Promise.all(promises).then((filelist) => {
+
+        filelist.sort((a, b) => {
+            let date1 = a.attributes.date;
+            let date2 = b.attributes.date;
+            if (date1 >= date2) return -1;
+            else return 1;
+        });
+
+        for (let i = 0; i < filelist.length; i += 1) {
+            const fm = frontmatter(filelist[i]);
+            const fileslug = fm.attributes.slug;
+            if (req.params.slug === fileslug) {
+                const html = converter.makeHtml(fm.body);
+                res.render('article', { html, title: fm.attributes.title });
+                found = true;
+            }
+        }
+        if (!found) {
+            res.status(404).render('error', { title: 'Fannst ekki.', error: 'Efnið fannst ekki.' });
+        }
+    })
+});
+
+app.get('/', async (req, res) => {
+    let filelist = await readdir('./articles');
+    filelist = filelist.filter(item => item.endsWith('.md'));
+
+    const attrlist = [];
+    for (let i = 0; i < filelist.length; i += 1) {
+        const data = await read(`./articles/${filelist[i]}`);
+        const fm = frontmatter(data);
+
+        const date = new Date(fm.attributes.date);
+        fm.attributes.date = date.toDateString();
+
+        attrlist[i] = fm.attributes;
+    }
+    res.render('frontpage.ejs', { articles: attrlist, title: 'Greinasafn' });
+});
+
+// async function sort(attrlist) {
+//     attrlist.sort((a, b) => {
+//         let date1,
+//             date2;
+//         date1 = await a.attributes.date;
+//         date2 = await b.attributes.date;
+//         if (date1 >= date2) return -1;
+//         return 1;
+//     });
+// }
+
+
+// Handle 404
+app.use((req, res) => {
+    res.render('error', { title: 'Fannst ekki.', error: 'Efnið fannst ekki.' });
+});
+
 //  // Handle 500
 //  app.use(function(error, req, res, next) {
 //     res.render('error',{title : "Obbossí",error : "Eitthvað fór úrskeiðis."})
 //  });
 
 // Exports
-module.exports = app
+module.exports = app;

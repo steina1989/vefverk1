7a646ee88f010e8212c170020d6aa8a103a01879
Steina <steina.dogg@gmail.com>
2018-02-04 15:17:28 +0000

    Finished async fixes

--- articles.js
+++ articles.js
@@ -15,7 +15,6 @@ const express = require('express');
 
 const app = express();
 
-
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
@@ -25,77 +24,63 @@ app.use('/articles/img', express.static('./articles/img/'));
 
 // Helpers
 async function read(file) {
-    const data = await readFile(file);
-    return data.toString();
+  const data = await readFile(file);
+  return frontmatter(data.toString());
 }
 
 
 // Routes
 app.get('/articles/:slug', async (req, res) => {
-    let filelist = await readdir('./articles');
-    filelist = filelist.filter(item => item.endsWith('.md'));
+  let filelist = await readdir('./articles');
+  filelist = filelist.filter(item => item.endsWith('.md'));
 
-    let promises = filelist.map(async (value) => {
-        return read(`./articles/${value}`);
-    })
+  const promises = filelist.map(async value => read(`./articles/${value}`));
 
-    let found = false;
-    Promise.all(promises).then((filelist) => {
-
-        filelist.sort((a, b) => {
-            let date1 = a.attributes.date;
-            let date2 = b.attributes.date;
-            if (date1 >= date2) return -1;
-            else return 1;
-        });
-
-        for (let i = 0; i < filelist.length; i += 1) {
-            const fm = frontmatter(filelist[i]);
-            const fileslug = fm.attributes.slug;
-            if (req.params.slug === fileslug) {
-                const html = converter.makeHtml(fm.body);
-                res.render('article', { html, title: fm.attributes.title });
-                found = true;
-            }
-        }
-        if (!found) {
-            res.status(404).render('error', { title: 'Fannst ekki.', error: 'Efnið fannst ekki.' });
-        }
-    })
+  let found = false;
+  Promise.all(promises).then((filelist) => {
+    for (let i = 0; i < filelist.length; i += 1) {
+      const fm = filelist[i];
+      const fileslug = fm.attributes.slug;
+      if (req.params.slug === fileslug) {
+        const html = converter.makeHtml(fm.body);
+        res.render('article', { html, title: fm.attributes.title });
+        found = true;
+      }
+    }
+    if (!found) {
+      res.status(404).render('error', { title: 'Fannst ekki.', error: 'Efnið fannst ekki.' });
+    }
+  });
 });
 
 app.get('/', async (req, res) => {
-    let filelist = await readdir('./articles');
-    filelist = filelist.filter(item => item.endsWith('.md'));
+  let filelist = await readdir('./articles');
+  filelist = filelist.filter(item => item.endsWith('.md'));
 
-    const attrlist = [];
-    for (let i = 0; i < filelist.length; i += 1) {
-        const data = await read(`./articles/${filelist[i]}`);
-        const fm = frontmatter(data);
+  const promises = filelist.map(async value => read(`./articles/${value}`));
 
-        const date = new Date(fm.attributes.date);
-        fm.attributes.date = date.toDateString();
+  Promise.all(promises).then((fmlist) => {
+    const attrList = [];
+    fmlist.sort((a, b) => {
+      const date1 = Date.parse(a.attributes.date);
+      const date2 = Date.parse(b.attributes.date);
+      if (date1 >= date2) return -1;
+      return 1;
+    });
 
-        attrlist[i] = fm.attributes;
+    for (let i = 0; i < fmlist.length; i += 1) {
+      const date = new Date(fmlist[i].attributes.date);
+      fmlist[i].attributes.date = date.toDateString();
+      attrList.push(fmlist[i].attributes);
     }
-    res.render('frontpage.ejs', { articles: attrlist, title: 'Greinasafn' });
+    res.render('frontpage.ejs', { articles: attrList, title: 'Greinasafn' });
+  });
 });
-
-// async function sort(attrlist) {
-//     attrlist.sort((a, b) => {
-//         let date1,
-//             date2;
-//         date1 = await a.attributes.date;
-//         date2 = await b.attributes.date;
-//         if (date1 >= date2) return -1;
-//         return 1;
-//     });
-// }
 
 
 // Handle 404
 app.use((req, res) => {
-    res.render('error', { title: 'Fannst ekki.', error: 'Efnið fannst ekki.' });
+  res.render('error', { title: 'Fannst ekki.', error: 'Efnið fannst ekki.' });
 });
 
 //  // Handle 500
